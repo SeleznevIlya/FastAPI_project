5b677dc702a97bedb33c60ff6e70f852cc2236b9
Helfik <helfik123_1998@mail.ru>
2023-01-29 19:33:03 +0300

    create getting single pereval

--- virtual_internship/app.py
+++ virtual_internship/app.py
@@ -14,6 +14,10 @@ def get_pereval_list(db: Session = Depends(get_db)):
     return crud.get_pereval_list(db)
 
 
+@router.get('/{pk}/')
+def get_pereval(pk: int, db: Session = Depends(get_db)):
+    return crud.get_pereval(db, pk)
+
 @router.post('/')
 def create_pereval(item: PerevalCreate, db: Session = Depends(get_db)):
     return crud.create_pereval(db, item)
--- virtual_internship/crud.py
+++ virtual_internship/crud.py
@@ -4,10 +4,17 @@ from .schemas import PerevalCreate
 
 
 def get_pereval_list(db: Session):
+    """Получаем список всех перевалов"""
     return db.query(Pereval).all()
 
 
+def get_pereval(db: Session, pk: int):
+    """"Получаем конкретный перевал по pk"""
+    return db.query(Pereval).get(pk)
+
+
 def create_pereval(db: Session, item: PerevalCreate):
+    """Создаем объект перевала"""
     pereval = Pereval(**item.dict())
     db.add(pereval)
     db.commit()
